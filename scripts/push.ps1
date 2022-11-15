05e81b7b58b92537ca2f4465cf14a374500906f5
Julio Cachay <jcachay@mktg33.com>
2022-11-15 15:12:22 -0500

    cp

--- scripts/push.ps1
+++ scripts/push.ps1
@@ -1,4 +1,4 @@
-﻿Get-ChildItem ../src/jcg.Domain/bin/Debug -Filter "*.nupkg" | ForEach-Object {
+﻿Get-ChildItem ./jcg.Domain/bin/Debug -Filter "*.nupkg" | ForEach-Object {
         Write-Host "push.ps1: Pushing $($_.Name)"
        # dotnet nuget push $_ --source $Env:NUGET_URL --api-key $Env:NUGET_API_KEY
        # if ($lastexitcode -ne 0) {
